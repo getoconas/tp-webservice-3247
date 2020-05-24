a427d7d33761237b070d56966c746783e2787958
getoconas <getoconas@gmail.com>
2020-05-23 22:02:04 -0300

    creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DivisasComponent } from './components/divisas/divisas.component';
+import { NoticiasComponent } from './components/noticias/noticias.component';
+import { HoroscopoComponent } from './components/horoscopo/horoscopo.component';
+import { CovidComponent } from './components/covid/covid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DivisasComponent,
+    NoticiasComponent,
+    HoroscopoComponent,
+    CovidComponent
   ],
   imports: [
     BrowserModule,
